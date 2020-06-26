a9631e273aa80a1459a6ed28101a6c64b8416189
Phil King <king.phil@gmail.com>
2020-06-26 21:55:36 +0000

    scaffolding for travis-ci
    minimal test for 'eieio-named' class

--- pekobj-tests.el
+++ pekobj-tests.el
@@ -1,10 +1,26 @@
 (require 'eieio-base)
 
-;; following convention of prefixing everything with "pekobj-"
-;; minimal amount of code to define and exercise a class
-(defclass pekobj-minimal-class (eieio-named) ())
+;; eieio-named
+(defclass pekobj-named (eieio-named) ())
 
-(ert-deftest pekobj-test-minimal-class ()
-  "Basic test of 'minimal' class"
-  (let ((pekobj (pekobj-minimal-class :object-name "explicitly-named")))
-    (should (equal (oref explicit :object-name) "explicitly-named"))))
+(ert-deftest test-pekobj-named ()
+  "Basic test of 'eieio-named' class"
+  (let* ((my-obj-name "pekobj-named")
+	 (my-obj-rename "pekobj-renamed")
+	 (pekobj (pekobj-named :object-name my-obj-name)))
+    (progn
+      (should (equal (oref pekobj :object-name) my-obj-name))
+      (should-error (eieio-object-set-name-string pekobj 'must-be-string) :type 'wrong-type-argument)
+      (eieio-object-set-name-string pekobj my-obj-rename)
+      (should (equal (oref pekobj :object-name) my-obj-rename)))))
+
+;; minimal syntax for a class
+(defclass pekobj-minimal () ())
+
+(cl-defmethod echo ((obj-name pekobj-minimal) arg)
+  arg)
+
+(ert-deftest test-pekobj-minimal ()
+  "Test for absolute minimum syntax"
+  (let ((pekobj (pekobj-minimal)))
+    (should (equal (echo pekobj "hellooooo!") "hellooooo!"))))

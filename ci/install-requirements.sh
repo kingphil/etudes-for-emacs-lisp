2832757b91c723acb354278b60f26d0bb6d30117
Philip King <king.phil@gmail.com>
2021-08-14 15:09:21 +0000

    run tests via ert-runner

--- ci/install-requirements.sh
+++ ci/install-requirements.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 cask install
-cask exec ert-runner help
--- ci/runtests.sh
+++ ci/runtests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-emacs -Q -batch -l pekobj-tests.el -l ring-tests.el -f ert-run-tests-batch-and-exit
+cask exec ert-runner
